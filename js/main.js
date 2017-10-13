75e304a5287559f2caf8af5b9486233fe86f5ff7
thisisatest1990 <32713611+thisisatest1990@users.noreply.github.com>
2017-10-13 14:30:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-        window.sr = new scrollReveal();
-
-        var winBreak = 5;
-        $(window).on("scroll", function() {
-            if ($(this).scrollTop() > winBreak) {
-                $("#navCont").addClass("whitebg in");
-            } else {
-                $("#navCont").addClass("whitebg in");
-            }
-        });
+window.sr = new scrollReveal();
